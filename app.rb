13a6049090e09689daa38a6191ef931805e21086
Sergio Marin <higher.vnf@gmail.com>
2013-11-30 22:03:00 -0430

    configuracion de mongoid y modelos

--- app.rb
+++ app.rb
@@ -1,5 +1,35 @@
+require 'rubygems'
+require 'bundler'
 require 'sinatra/base'
+require 'json'
+require 'mongoid'
+
+Mongoid.load!("config/mongoid.yml")
+
+# Load every app file, controllers and models
+%w(libs/*.rb app/models/*.rb).each do |dir|
+  Dir.glob(dir).each do |file|
+    require_relative file
+  end
+end
 
 class App < Sinatra::Base
+  # include SessionsHelper
+  # enable :protection , :except => :ip_spoofing
+  configure do
+    enable :sessions
+    set :port, 3000
+    set :session_secret, 'asdfa2342923422f1adc05c837fa234230e3594b93824b00e930ab0fb94b'
+    set :root, File.dirname(__FILE__)
+    set :protection, :except => [:remote_token, :frame_options, :http_origin]
+    enable :logging, :dump_errors, :raise_errors
+  end
+  # Cross Domain AJAX configuration and API need this
+  before do
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DELETE, PUT'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type,X-Requested-With, X-Prototype-Version, X-CSRF-Token, Origin, Accept'
+  end
 
+  run! if app_file == $0
 end
